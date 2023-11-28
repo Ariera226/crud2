2d8f345db50063c2b14117fa7ebf3014fa79bd06
테스트 <a82605167@gmail.com>
2023-11-28 11:19:33 +0900

    First Crud

---
+++ components/TopicsList.jsx
@@ -0,0 +1,46 @@
+import Link from 'next/link';
+import RemoveBtn from './RemoveBtn';
+import { HiPencilAlt } from 'react-icons/hi';
+
+const getTopics = async () => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/topics`, {
+      cache: 'no-store',
+    });
+    if (!res.ok) {
+      throw new Error('Failed to fetch topics');
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default async function TopicsList() {
+  const { topics } = await getTopics();
+  return (
+    <ul>
+      {topics.map((topic) => (
+        <li
+          key={topic._id}
+          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
+        >
+          <div>
+            <h2 className="text-2xl font-bold"> {topic.title}</h2>
+            <div>{topic.description}</div>
+            <div className="flex flex-row gap-5">
+              <div>Created: {topic.createdAt}</div>
+              <div>Updated: {topic.updatedAt}</div>
+            </div>
+          </div>
+          <div className="flex gap-2">
+            <RemoveBtn id={topic._id} />
+            <Link href={`/editTopic/${topic._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
